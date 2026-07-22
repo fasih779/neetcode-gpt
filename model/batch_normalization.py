38154d1e64a832f30a30c155a9765961ed56c257
FasihMalhi <malhifasih1@gmail.com>
2026-07-22 07:40:39 -0700

    Implement Batch normalization (PyTorch 3/4)

---
+++ model/batch_normalization.py
@@ -0,0 +1,43 @@
+import numpy as np
+import torch
+from typing import Tuple, List
+
+
+class Solution:
+    def batch_norm(self, x: List[List[float]], gamma: List[float], beta: List[float],
+                   running_mean: List[float], running_var: List[float],
+                   momentum: float, eps: float, training: bool) -> Tuple[List[List[float]], List[float], List[float]]:
+        x = np.array(x)
+        gamma = np.array(gamma)
+        beta = np.array(beta)
+        running_mean = np.array(running_mean, dtype=np.float64)
+        running_var = np.array(running_var, dtype=np.float64)
+        # During training: normalize using batch statistics, then update running stats
+        if training:
+
+            mean = np.mean(x, axis=0)
+
+            var = np.mean((x - mean)**2, axis=0)
+
+            x_hat = (x - mean) / np.sqrt(var + eps)
+
+            out = gamma * x_hat + beta
+
+            running_mean = (
+            (1-momentum) * running_mean
+            + momentum * mean
+            )
+
+            running_var = (
+            (1-momentum) * running_var
+            + momentum * var
+            )
+
+        else:
+
+            x_hat = (x-running_mean) / np.sqrt(running_var + eps)
+
+            out = gamma*x_hat + beta
+
+
+        return (np.round(out, 4).tolist(), np.round(running_mean, 4).tolist(), np.round(running_var, 4).tolist())
